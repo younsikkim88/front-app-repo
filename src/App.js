60dbb1c2a98d84507d83aba1f09e4a8485b33e2e
EC2 Default User <ec2-user@ip-172-31-29-191.ap-northeast-2.compute.internal>
2022-02-25 07:18:29 +0000

    test

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog Version 1
+            EKS DEMO Blog Version 12
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
